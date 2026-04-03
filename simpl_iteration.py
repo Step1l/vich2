a8c0f62af28f30892d62f7bcfc6ea9112a61c832
Step1l <stilya2006@gmail.com>
2026-04-03 07:47:50 +0300

    2

--- simpl_iteration.py
+++ simpl_iteration.py
@@ -1,15 +1,5 @@
-def simple_iteration_method(f,df, a, b, eps):
+def simple_iteration_method(f, a, b, eps,phi):
     max_iter = 10000
-    xs = [a + (b - a) * i / 10 for i in range(11)]
-    max_df = max([abs(df(x)) for x in xs])
-    lambd = 1/max_df
-    phi_der = lambda x: 1 + lambd * df(x)
-    if df(a)>0:
-        lambd = -lambd
-    phi = lambda x: x+lambd*f(x)
-    for x in xs:
-        if abs(phi_der(x))>=1:
-            raise ValueError(f"Условие сходимости не выполняется, {x,phi(x),lambd}")
     x0 = (a+b)/2
     for i in range(max_iter):
         x1=phi(x0)
